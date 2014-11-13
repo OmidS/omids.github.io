39fc18f542ef60ca791370b81f645e3644d7820a
OmidS <omidsani@gmail.com>
2014-11-13 16:52:13 +0330

    Initial migration to github

---
+++ ssvep/indexf.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html><head>
+<script type="text/javascript" src="js/jquery-1.10.2.min.js"></script>
+<!--<script type="text/javascript" src="js/jquery-migrate-1.0.0.min.js"></script>-->
+<!--<script type="text/javascript" src="js/jquery-migrate-1.0.0.js"></script>-->
+<script type="text/javascript" src="js/jquery.cookie.js"></script>
+<title>BCI</title>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+<link type="text/css" rel="stylesheet" href="style.css">
+<script type="text/javascript" src="js/stim.js"></script>
+<script type="text/javascript">
+  var _gaq = _gaq || [];
+  _gaq.push(['_setAccount', 'UA-36524394-1']);
+  _gaq.push(['_setDomainName', 'sharif.edu']);
+  _gaq.push(['_setAllowLinker', true]);
+  _gaq.push(['_trackPageview']);
+  (function() {
+    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+  })();
+</script>
+</head>
+<body>
+<div class="setupPage"></div>
+<div class="stimulator displayNone"></div>
+<script type="text/javascript">
+setupStimulator();
+</script>
+</body>
+</html>
